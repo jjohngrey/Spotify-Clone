71449ec86210f515b533d50aaf4984e77dfad4c2
jjohngrey <john1grey9@gmail.com>
2024-07-05 22:36:48 -0700

    update stripe api version

--- libs/stripe.ts
+++ libs/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 export const stripe = new Stripe(
     process.env.STRIPE_SECRET_KEY ?? '',
     {
-        apiVersion: '2022-11-15',
+        apiVersion: '2024-04-10',
         appInfo: {
             name: 'Spotify Clone',
             version: '0.1.0'
